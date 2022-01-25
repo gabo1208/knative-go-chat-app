ed4411c44ad783fec1114b1f315cc872c7a2e1eb
gabriel <gfreites@vmware.com>
2022-01-25 16:02:33 -0500

    added reconnecting websockets dependencies and added first websockets connection logic when rendering the dashboard

--- web/src/components/dashboard/Dashboard.js
+++ web/src/components/dashboard/Dashboard.js
@@ -1,15 +1,17 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import './Dashboard.css'
 import { Contacts } from './contacts/Contacts'
 import { Chat } from './chat/Chat'
 import { Menu } from './menu/Menu'
 import { DetectSmallScreenWidth } from '../../utils/Helper'
+import ReconnectingWebSocket from 'reconnecting-websocket';
 
 export function Dashboard(props) {
   const [chatState, setChatState] = useState({
     selectedChatIndex: -1,
-    connectedUsers: []
-    // This while I implement typescript
+    connectedUsers: [],
+    open: false
+    // This while I change to typescript
     // userModel: {username: string, messages: {content: string, mine: bool}}
   })
 
@@ -70,6 +72,87 @@ export function Dashboard(props) {
           updateChatMessagesCallback={updateChatMessagesCallback}
         />}
     </div>)
+  }
+
+  const ConnectToWebSocket = useEffect(() => {
+    console.log("Protocol: " + window.location.protocol);
+    let wsURL = "ws://" + document.location.host + "/ws";
+    if (window.location.protocol === 'https:') {
+      wsURL = "wss://" + document.location.host + "/ws";
+    }
+
+    wsURL = "ws://sockeye.default.20.190.7.108.xip.io/ws";
+
+    console.log("WS URL: " + wsURL);
+
+    let that = this;
+
+    let sock = new ReconnectingWebSocket(wsURL);
+    sock.onopen = function () {
+      console.log("connected to " + wsURL);
+      //let fab = document.getElementById("fab");
+      //fab.setAttribute("sockeye-connected", "true");
+    };
+    sock.onclose = function (e) {
+      console.log("connection closed (" + e.code + ")");
+      //fab.setAttribute("sockeye-connected", "false");
+    };
+    sock.onmessage = function (e) {
+      window.dispatchEvent(new Event('cloudevent'));
+      let t = JSON.parse(JSON.parse(e.data)); // at the moment the ws sends down a double encoded thing.
+
+      console.log(t)
+      that.onCloudEvent(t)
+    }
+  })
+
+  const showError = () => (this.setState({ open: !this.state.open }))
+
+  const onCloudEvent = (event) => {
+    let data = { id: event.id };
+
+    Object.keys(event).forEach(key => {
+      if (key === "data") {
+        data[key] = JSON.stringify(event[key]);
+        return;
+      }
+      data[key] = event[key];
+    });
+
+    let al = [...this.state.events];
+
+    if (this.state.revert) {
+      if (data["data"] != null) {
+        al.push(data);
+        this.setState({
+          events: al.reverse()
+        });
+        return;
+      }
+      if (data["data"] === undefined) {
+        this.showError();
+        console.log("More information on the invalid event: ", event);
+        return;
+      }
+
+    }
+
+    if (!this.state.revert) {
+      if (data["data"] != null) {
+        al.push(data);
+        this.setState({
+          events: al
+        });
+        return;
+      }
+      if (data["data"] === undefined) {
+        this.showError();
+        console.log("More information on the invalid event: ", event);
+        return;
+      }
+
+
+    }
   }
 
   return (
